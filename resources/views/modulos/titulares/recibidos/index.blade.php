5d7333fec0e03b67fbfc3b4b6edbd72a9d25af50
zandroallan <zandroallan@gmail.com>
2024-05-07 15:51:33 -0600

    Actualizaciones de vista

--- resources/views/modulos/titulares/recibidos/index.blade.php
+++ resources/views/modulos/titulares/recibidos/index.blade.php
@@ -28,7 +28,6 @@
 
     @section('content')
 
-
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
